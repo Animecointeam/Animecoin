695b356b1fe13243e52c988eb14818ff4dd94b4d
Caraphinneth <daiyousei@pm.me>
2019-05-31 14:15:50 +0300

    Various small fixes.

--- src/rpcserver.cpp
+++ src/rpcserver.cpp
@@ -316,6 +316,7 @@ static const CRPCCommand vRPCCommands[] =
   { "generating",         "getgenerate",            &getgenerate,            true,      false,      false },
   { "generating",         "gethashespersec",        &gethashespersec,        true,      false,      false },
   { "generating",         "setgenerate",            &setgenerate,            true,      true,       false },
+  { "generating",         "generate",               &generate,               true,      false,      false },
 #endif
 
     /* Raw transactions */
