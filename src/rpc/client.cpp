2b63e2792f962f040b0f03ad3171b055f7489b8d
Caraphinneth <daiyousei@pm.me>
2019-08-05 01:26:06 +0300

    Ancestor tracking: add RPC calls.

--- src/rpc/client.cpp
+++ src/rpc/client.cpp
@@ -99,6 +99,8 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "prioritisetransaction", 2 },
     { "setban", 2 },
     { "setban", 3 },
+    { "getmempoolancestors", 1 },
+    { "getmempooldescendants", 1 },
 };
 
 class CRPCConvertTable
