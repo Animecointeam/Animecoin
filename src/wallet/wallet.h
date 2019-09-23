34d7a3dd75d5582aee004698d47f929e067d6623
Caraphinneth <daiyousei@pm.me>
2019-09-23 05:55:44 +0300

    Correct versioning.

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -80,9 +80,9 @@ enum WalletFeature
     FEATURE_WALLETCRYPT = 40000, // wallet encryption
     FEATURE_COMPRPUBKEY = 60000, // compressed public keys
 
-    FEATURE_HD = 130000, // Hierarchical key derivation after BIP32 (HD Wallet)
+    FEATURE_HD = 100000, // Hierarchical key derivation after BIP32 (HD Wallet)
 
-    FEATURE_HD_SPLIT = 139900, // Wallet with HD chain split (change outputs will use m/0'/1'/k)
+    FEATURE_HD_SPLIT = 100000, // Wallet with HD chain split (change outputs will use m/0'/1'/k)
 
     FEATURE_LATEST = FEATURE_COMPRPUBKEY // HD is optional, use FEATURE_COMPRPUBKEY as latest version
 };
