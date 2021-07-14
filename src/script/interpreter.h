33dbcfe9d03b595d11b843470494ea7a0d16605e
Caraphinneth <daiyousei@pm.me>
2021-07-14 12:53:35 +0300

    Skip precomputing sighashes for non-witness txs.

--- src/script/interpreter.h
+++ src/script/interpreter.h
@@ -105,6 +105,7 @@ bool CheckSignatureEncoding(const std::vector<unsigned char> &vchSig, unsigned i
 struct PrecomputedTransactionData
 {
     uint256 hashPrevouts, hashSequence, hashOutputs;
+    bool ready = false;
 
     PrecomputedTransactionData(const CTransaction& tx);
 };
