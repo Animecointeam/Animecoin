8cedf154d65ac13c059cdf11e2e0d26e853cf46a
Caraphinneth <daiyousei@pm.me>
2024-10-12 02:57:10 +0300

    Add miniupnpc 2.2.8 support.

--- src/net.cpp
+++ src/net.cpp
@@ -1440,7 +1440,11 @@ void ThreadMapPort()
     struct IGDdatas data;
     int r;
 
+#if MINIUPNPC_API_VERSION <= 17
     r = UPNP_GetValidIGD(devlist, &urls, &data, lanaddr, sizeof(lanaddr));
+#else
+    r = UPNP_GetValidIGD(devlist, &urls, &data, lanaddr, sizeof(lanaddr), nullptr, 0);
+#endif
     if (r == 1)
     {
         if (fDiscover) {
