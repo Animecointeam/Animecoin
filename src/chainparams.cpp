976d37f38536d7d01687c072c7c2efaebb4e0a4b
Caraphinneth <daiyousei@pm.me>
2024-10-13 22:56:55 +0300

    Postpone segwit activation till Oct 31st 2025.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -110,7 +110,7 @@ public:
 
         // Deployment of SegWit (BIP141 and BIP143)
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].bit = 1;
-        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1646092800; // Mar 1st, 2022
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1761868800; // Oct 31st, 2025
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = Consensus::BIP9Deployment::NO_TIMEOUT; // May 1st, 2062
 
         /**
